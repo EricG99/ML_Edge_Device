bd5baf18a75bd5d9e7b309e6630e0b632566a741
Lukas Baur <lukas.baur@ipa.fraunhofer.de>
2025-07-21 14:56:15 +0200

    config

--- config/config_general.py
+++ config/config_general.py
@@ -31,7 +31,7 @@ MQTT_CONFIG = {
 # Die Logik, welcher Run geladen wird, sollte zur Laufzeit entschieden werden,
 # aber die statischen Namen für den "Fast-Mode" können hier bleiben.
 CONFIG_LOAD_ARTIFACTS = {
-    "inference_mode": "load_artifacts_path", # "load_artifacts_fast" oder "load_artifacts_path"
+    "inference_mode": "load_artifacts_fast", # "load_artifacts_fast" oder "load_artifacts_path"
     "inference_steps" : 100,
     "inference_interval_sec": 0.5,
     "artifacts_base_path": ARTIFACTS_OUTPUT_PATH,
