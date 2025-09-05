af2b48300bed51a71e1c2c4e964180c70bf62923
Lukas Baur <lukas.baur@ipa.fraunhofer.de>
2025-09-05 18:37:32 +0200

    edge

--- experiment/experiment_pipeline_multiconfig.py
+++ experiment/experiment_pipeline_multiconfig.py
@@ -219,7 +219,7 @@ MODEL_BLOBS_WHITELIST = {
     "sklearn": ["model.joblib"],
     "xgb": ["model.json"],
 }
-SUMMARY_CSV_NAME = "Experiment_Summary_Server_multiconfig_edge.csv"
+SUMMARY_CSV_NAME = "Experiment_Summary__multiconfig_edge_Full.csv"
 SCALER_FILE_NAMES = ["scaler.joblib", "y_scaler.joblib"]
 
 # ---------------------------
